a732ae9cb712e090d4799550e11bd64b816e2c77
Devin Klepp <djklepp@gmail.com>
2025-04-05 17:58:48 -0400

    Implements chapter 2

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 My implementation of [Ray Tracing in One Weekend](https://raytracing.github.io/books/RayTracingInOneWeekend.html) in Rust
 
 This is my first attempt at a Rust project, and it combines my interests in learning more about graphics and video graphics, as well as the Rust language itself.
+
+To run:
+
+```bash
+cargo run > image.ppm
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
-    println!("Hello, world!");
+    // Image
+    let image_width: u32 = 256;
+    let image_height: u32 = 256;
+
+    // Render (PPM File Format)
+    println!("P3");
+    println!("{} {}", image_width, image_height);
+    println!("255");
+
+    for j in 0..image_height {
+        eprint!("\r\x1B[KScanlines remaining {}", image_height - j - 1);
+        for i in 0..image_width {
+            let r: f64 = i as f64 / (image_width - 1) as f64;
+            let g: f64 = j as f64 / (image_height - 1) as f64;
+            let b: f64 = 0.0;
+
+            let ir: u32 = (255.999 * r) as u32;
+            let ig: u32 = (255.999 * g) as u32;
+            let ib: u32 = (255.999 * b) as u32;
+
+            println!("{} {} {}", ir, ig, ib);
+        }
+    }
+    eprintln!("\nDone.");
 }
